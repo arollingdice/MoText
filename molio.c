1a3801ab0200203e938e83088509ff5f10968841
arollingdice <arollingdice@gmail.com>
2023-03-25 21:02:50 -0600

    now it draws the correct lines of tildes on the left side of the screen.

--- molio.c
+++ molio.c
@@ -1,38 +1,299 @@
+/*** includes ***/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
-struct termios orig_termios;
+/*** defines ***/
+#define MOLIO_VERSION "0.0.1"
+
+#define CTRL_KEY(k) ((k)&0x1f)
+
+/*** data ***/
+
+// global struct to store the current state of
+// our editor
+struct editorConfig
+{
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
+
+/*** terminal ***/
+
+void die(const char *s)
+{
+    // if error happens, clear the screen first
+    // then print the error message
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    perror(s);
+    exit(1);
+}
 
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
+        die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    struct termios raw = E.orig_termios;
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
+}
+
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+
+    return c;
+}
+
+/*
+    It looks like it's setting and cols just by
+    looking at the function parameters.
+
+    But actually the function returns ws's col and ws's row
+    values to input params' pointers.
+    In this way, you actually GET the values of rows and cols of
+    a terminal's size.
+
+    On success, we get 0. On failed, we get -1.
+
+*/
+
+int getCursorPosition(int *rows, int *cols)
+{
+    char buf[32];
+    unsigned int i = 0;
+
+    // n is for quering the terminal for status information
+    // 6 is for asking the cursor position
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+
+    while (i < sizeof(buf) - 1)
+    {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if (buf[i] == 'R')
+            break;
+        i++;
+    }
+    // make sure the end of the string is goddamn fucking '\0'
+    buf[i] = '\0';
+
+    // the first byte of char *buf is an escape character,
+    // so it wont display on the terminal.
+    // printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+
+    if (buf[0] != '\x1b' || buf[1] != '[')
+        return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+        return -1;
+
+    return 0;
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;
+
+        return getCursorPosition(rows, cols);
+    }
+    else
+    {
+        // common approach to having functions
+        // return multiple values
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+/*** append buffer ***/
+
+struct abuf
+{
+    char *b;
+    int len;
+};
+
+// {NULL, 0} is a struct. LOL!
+#define ABUF_INIT {NULL, 0};
+
+/*
+
+This function appends a string s of length len to a dynamic buffer
+represented by the abuf struct pointed to by ab.
+
+The function first attempts to resize the buffer to accommodate the new string using the realloc() function,
+which returns a pointer to the newly resized buffer. If realloc() returns NULL,
+the function returns without modifying the buffer.
+
+If the buffer was successfully resized, the function then uses the memcpy() function to copy the contents
+of the input string s to the end of the buffer,  starting at the current length of the buffer ab->len.
+Finally, the function updates the ab->b pointer to point to the newly resized buffer
+and increments the ab->len variable to reflect the new length of the buffer
+
+*/
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL)
+        return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+    free(ab->b);
+}
+
+/*** output ***/
+
+void editorDrawRows(struct abuf *ab)
+{
+    int y;
+    for (y = 0; y < E.screenrows; y++)
+    {
+        if (y == E.screenrows / 3)
+        {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                                      "Molio Editor -- version %s", MOLIO_VERSION);
+            if (welcomelen > E.screencols)
+                welcomelen = E.screencols;
+            // To center a string, you divide the screen width by 2, 
+            // and then subtract half of the string’s length from that
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding)
+            {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--)
+                abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        }
+        else
+        {
+            abAppend(ab, "~", 1);
+        }
+        // clear each line as we redraw them.
+        // K command erases part of the current line.
+        // 0 here erases the part of the line right of the cursor.
+        abAppend(ab, "\x1b[k", 3);
+        if (y < E.screenrows - 1)
+        {
+            abAppend(ab, "\r\n", 2);
+        }
+    }
+}
+
+void editorRefreshScreen()
+{
+    /*
+    \x1b is the escape charcter. It's 27 in decimal.
+    I am writing 4 bytes out to the terminal
+    escape character is always followed by a [ character
+
+    Escape sequences instruct the terminal to do various text
+    like formatting tasks, such as coloring text,
+    moving the cursor around, and clearing parts of the screen.
+
+    The J command is to clear the screen.
+    The 2 is an argument before the command, which says clear
+    the entire screen. 1 would clear the screen up to where
+    the cursor is. 0 is the default argument for J, which would
+    clear the screen from the cursor up to the end of the screen.
+    */
+    struct abuf ab = ABUF_INIT;
+
+    abAppend(&ab, "\x1b[?25l", 6); // this is for hiding the cursor before refreshing the screen
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25l", 6); // this is for showing the cursor immediately when the refresh is done.
+
+    write(STDERR_FILENO, ab.b, ab.len);
+    abFree(&ab);
+}
+
+/*** input ***/
+
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
+        exit(0);
+        break;
+    }
+}
+
+/*** init ***/
+
+void initEditor()
+{
+    // when you pass in E.screenrows and &E.screencols
+    // it actually set the values for them, hence "init".
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+        die("getWindowSize");
 }
 
 int main()
 {
     enableRawMode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
-        if(iscntrl(c)){
-            printf("%d\n", c);
-        }else{
-            printf("%d ('%c')\n", c, c);
-        }
+    initEditor();
 
+    while (1)
+    {
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
 
     return 0;
